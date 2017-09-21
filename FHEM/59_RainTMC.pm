5b3d8073321c80a06b4c161bbb5b3660ac799591
LuBeDa <lb@sell-e.de>
2017-09-21 19:38:50 +0200

    fv

--- FHEM/59_RainTMC.pm
+++ FHEM/59_RainTMC.pm
@@ -166,8 +166,12 @@ sub RainTMC_ParseHttpResponse($) {
     elsif ( $data ne "" ) {
         Log3( $name, 3, "$name: returned: $data" );
         my $decoded_json = decode_json($data);
+        
         Log3 ($name,3,$decode_json->{ForecastResult});
         
+        foreach my $entry in ($decode_json->{ForecastResult}) {
+            Log3 ($name,3,$entry->{value});
+        }
         my $rainamount    = 0.0;
         my $rainbegin     = "unknown";
         my $rainend       = "unknown";
