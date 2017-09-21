921b7352e94c59221a339c8c44f197e1db0d154f
LuBeDa <lb@sell-e.de>
2017-09-21 20:22:30 +0200

    f

--- FHEM/59_RainTMC.pm
+++ FHEM/59_RainTMC.pm
@@ -171,7 +171,7 @@ sub RainTMC_ParseHttpResponse($) {
         Log3 ($name,3,$decoded_json->{ForecastResult});
         foreach $entry ($decoded_json->{ForecastResult}) 
         {
-            Log3 ($name,3, $entry->{value}. " " . $entry->{TimeStamp});
+            Log3 ($name,3, Dumper($entry);
         }
                
         my $rainamount    = 0.0;
