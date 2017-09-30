d5c8ada5ab1b25d0ea7341fc07a58290ae2ceadd
LuBeDa <lb@sell-e.de>
2017-09-30 20:50:25 +0200

    g

--- FHEM/59_RainTMC.pm
+++ FHEM/59_RainTMC.pm
@@ -234,7 +234,7 @@ sub RainTMC_ParseHttpResponse($) {
                      $as_htmlhead .= "<td>&nbsp;</td>"
                 }
                 if (($a->{ColorAsRGB} eq "Transparent")||($rain==0)) {
-                $as_html .= '<td bgcolor=#ffffff">&nbsp;</td>';
+                $as_html .= '<td bgcolor="#ffffff">&nbsp;</td>';
                 } else{
                     $as_html .= '<td bgcolor="'. $a->{ColorAsRGB} .'">&nbsp;</td>';
                 }
